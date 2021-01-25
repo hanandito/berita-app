a4264145b76ac8c18d48915003cc4a16b31fdbbf
teddypicker <hananditos@gmail.com>
2021-01-25 14:25:11 +0700

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
   },
 
   staging: {
