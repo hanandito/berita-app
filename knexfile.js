f46d236ef25f28dc3b90e37e343a728fa1655327
teddypicker <hananditos@gmail.com>
2021-01-25 14:26:10 +0700

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'mysql',
   connection: {
     host : '127.0.0.1',
-    user : 'your_database_user',
+    user : 'root',
     password : 'your_database_password',
     database : 'myapp_test'
   }
